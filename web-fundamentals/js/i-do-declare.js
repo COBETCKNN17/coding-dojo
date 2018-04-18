2519d16f90fb863ace750c7b03ea30a23940c996
Nat <nuku@milkgoblin.com>
2018-04-18 17:10:40 -0500

    I Do Declare
    Javascript exercise added

---
+++ web-fundamentals/js/i-do-declare.js
@@ -0,0 +1,27 @@
+function iDoDeclare(){
+
+    var num1 = 63;
+    var num2 = -2;
+    var num3 = 333;
+    var num4 = 43980324.7;
+    var str1 = 'Wow!';
+    var str2 = 'Cool!';
+    var str3 = 'Running out of things to say...';
+    var boo1 = true;
+    var boo2 = false;
+    var what;
+
+    console.log("The First Number:", num1);
+    console.log("The Second Number:", num2);
+    console.log("The Third Number:", num3);
+    console.log("The Fourth Number:", num4);
+    console.log("String One:", str1);
+    console.log("String Two:", str2);
+    console.log("String Three:", str3);
+    console.log("Boolean One:", boo1);
+    console.log("Boolean Two:", boo2);
+    console.log("Undefined:", what);
+
+}
+
+iDoDeclare();
