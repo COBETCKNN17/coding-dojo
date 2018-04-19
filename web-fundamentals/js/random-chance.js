91ff378aec8a4d361c5d1b4af890b0219468e721
Nat <nuku@milkgoblin.com>
2018-04-19 14:48:18 -0500

    Random Chance
    Function to play a slot machine that takes a number of quarters (1 quarter = 1 game).
    
    There is a 1 in 100 chance to win the slot machine (which will give you between 50 - 100 quarters)

---
+++ web-fundamentals/js/random-chance.js
@@ -0,0 +1,28 @@
+function randomChance(quarters){
+    var count = 0;
+    var random = 0;
+
+    console.log ("You have", quarters, "quarters. You put 1 quarter in the slot machine.");
+
+    while (quarters > 0){
+
+        quarters--;
+        var random = Math.floor((Math.random() *100) + 1);
+
+        if(random == 33){
+            var quarters = quarters + Math.trunc(Math.random() * 50)+51;
+            console.log("WINNER!!! You now have", quarters,"quarters.");
+        }
+
+        else{
+            console.log("You lost... You now have", quarters,"quarters.");
+        }
+    }
+
+    if(quarters == 0){
+        console.log("Game Over. You have run out of quarters.");
+        return 0;
+    }
+}
+
+randomChance(10);
