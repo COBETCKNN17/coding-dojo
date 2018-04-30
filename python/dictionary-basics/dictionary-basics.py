d6405815a88b6c56d07ecf0a4fadb172b38d9845
Nat <nuku@milkgoblin.com>
2018-04-30 17:35:22 -0500

    Dictionary Basics

---
+++ python/dictionary-basics/dictionary-basics.py
@@ -0,0 +1,14 @@
+def aboutMe ():
+
+    info = {}
+    info["name"] = "Nat"
+    info["age"] = "29"
+    info["country"] = "USA"
+    info["language"] = "Python"
+
+    print "My name is", info["name"]
+    print "My age is", info["age"]
+    print "My country of birth is", info["country"]
+    print "My favorite language is", info["language"]
+
+aboutMe()
