35aff0bb14008d152cde324fed274aa09f664702
Nat <nuku@milkgoblin.com>
2018-04-30 18:03:54 -0500

    Making Dictionaries
    Set up the outline for this assignment, not yet started.

---
+++ python/making-dictionaries/making-dictionaries.py
@@ -0,0 +1,8 @@
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+def make_dict(list1, list2):
+    new_dict = {}
+    # your code here
+    return new_dict
+make_dict(name, favorite_animal)
