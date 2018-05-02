901855f5b6c39623b4bcdc5b7643cb211f969497
Nat <nuku@milkgoblin.com>
2018-05-02 13:40:00 -0500

    Changed 'total' to 'result'
    Changed variable total to result to match more closely with the assignment's request

--- python/math-dojo/math-dojo.py
+++ python/math-dojo/math-dojo.py
@@ -4,23 +4,23 @@
 
 class MathDojo(object):
     def __init__(self):
-        self.total = 0
+        self.result = 0
 
     def add(self, *x):
         self.addNum = 0
         for value in x:
             self.addNum += value
-        self.total += self.addNum
+        self.result += self.addNum
         return self
 
     def subtract(self, *y):
         self.subNum = 0
         for value in y:
             self.subNum += value
-        self.total -= self.subNum
+        self.result -= self.subNum
         return self
 
-md = MathDojo().add(2).add(2,5).subtract(3,2).total
+md = MathDojo().add(2).add(2,5).subtract(3,2).result
 print md
 
 '''
@@ -28,25 +28,25 @@ print md
 
 class MathDojo(object):
     def __init__(self):
-        self.total = 0
+        self.result = 0
 
     def add(self, *x):
         for i in range(0, len(x)):
             if type(x[i]) is tuple or type(x[i]) is list:
                 for num in x[i]: 
-					self.total += num
+				    self.result += num
             else:
-                self.total += x[i]
+                self.result += x[i]
         return self
 
     def subtract(self, *y):
         for i in range(0, len(y)):
             if type(y[i]) is tuple or type(y[i]) is list:
                 for num in y[i]: 
-					self.total -= num
+				    self.result -= num
             else:
-                self.total -= y[i]
+                self.result -= y[i]
         return self
 
-md = MathDojo().add([1], 3,4).add([3,5,7,8], (2,4.3,1.25)).subtract(2, [2,3], [1.1,2.3]).total
+md = MathDojo().add([1], 3,4).add([3,5,7,8], (2,4.3,1.25)).subtract(2, [2,3], [1.1,2.3]).result
 print md
