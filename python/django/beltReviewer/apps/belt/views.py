1173ed30cfdc695480016189d8ab8ded3c70fd30
Nat <nuku@milkgoblin.com>
2018-05-16 14:59:15 -0500

    Belt Reviewer
    Got bcrypt working, added login function

--- python/django/beltReviewer/apps/belt/views.py
+++ python/django/beltReviewer/apps/belt/views.py
@@ -1,14 +1,38 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
-from .models import *
+from .models import User
+import re
+from django.contrib import messages
+import bcrypt
 
 def index(request):
     return render (request, "belt/index.html")
 
 def register(request):
-    if request.method == "POST":
-        User.objects.register(request.POST)
-        return redirect("/")
+	if request.method == "POST":
+		User.objects.register(request)
+		return redirect("/")
+	else:
+		return redirect("/")
 
 def login(request):
-    pass
+    try:
+        user = User.objects.get ( email=request.POST["email"] )
+
+        isValid = bcrypt.checkpw( request.POST["password"].encode() , user.password.encode() )
+        
+        if isValid:
+            print ("Password match!")
+            return redirect ("/books")
+
+        else:
+            print ("Passwords do NOT match!")
+            messages.add_message (request, messages.ERROR, "Invalid Credentials!")
+            return redirect("/")
+        
+    except:
+        messages.add_message (request, messages.ERROR, "No user with this email found!")
+        return redirect("/")
+
+def books(request):
+    return render (request, "belt/books.html")
