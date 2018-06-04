8d4c665bbfe813211fb313819a8a689925789ee2
Nat <nuku@milkgoblin.com>
2018-06-04 16:06:04 -0500

    Brainstorming

--- MEAN/javascript/min-heap-insert.js
+++ MEAN/javascript/min-heap-insert.js
@@ -1,3 +1,5 @@
+// Not sure if this works. I'm assuming that a parent and index were both assigned during the construction of the heap like with nodes. Can I just treat it like a normal array instead? Need to revisit this.
+
 function minHeapInsert(arr, val){
     arr.push(val);
     var thischild = arr[arr.length-1];
@@ -10,4 +12,18 @@ function minHeapInsert(arr, val){
 
 minHeapInsert([2,3,4,5], 1)
 
-// Not sure if this works. I'm assuming that a parent and index were both assigned during the construction of the heap like with nodes. Can I just treat it like a normal array instead? Need to revisit this.
+// Assuming it works like a normal array:
+
+function minHeapInsertNew(arr, val){
+    arr.push(val);
+    var current = arr.length-1;
+    while (arr[current] < arr[current-1]){
+        temp = arr[current];
+        arr[current] = arr[current-1];
+        arr[current-1] = temp;
+        current--;
+    }
+  console.log(arr);
+}
+
+minHeapInsertNew([2,3,4,5], 1)
