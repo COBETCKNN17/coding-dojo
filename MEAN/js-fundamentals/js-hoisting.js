da30f74b4184ceca1e5d640ce67db95b3621d13d
Nat <nuku@milkgoblin.com>
2018-06-04 13:58:16 -0500

    JS Hoisting

---
+++ MEAN/js-fundamentals/js-hoisting.js
@@ -0,0 +1,78 @@
+//1
+var hello;
+var hello = 'world';
+console.log(hello);
+
+//2
+var needle;
+function test(){
+    var needle;
+	var needle = 'magnet';
+	console.log(needle);
+}
+var needle = 'haystack';
+test();
+
+//3
+var brendan;
+function print(){
+    var brendan;
+	brendan = 'only okay';
+	console.log(brendan);
+}
+var brendan = 'super cool';
+console.log(brendan);
+
+//4
+var food;
+function eat(){
+    var food;
+    var food = 'gone';
+    food = 'half-chicken';
+    console.log(food);
+    console.log(food);
+}
+var food = 'chicken';
+eat();
+console.log(food);
+
+//5
+
+var mean = function() {
+    var food;
+    var food = "fish";
+	food = "chicken";
+	console.log(food);
+	console.log(food);
+}
+console.log(food);
+console.log(food);
+mean();
+
+//6
+var genre;
+function rewind() {
+    var genre;
+    var genre = "r&b";
+	genre = "rock";
+	console.log(genre);
+	console.log(genre);
+}
+var genre = "disco";
+console.log(genre);
+rewind();
+console.log(genre);
+
+//7
+var dojo;
+dojo = "san jose";
+function learn() {
+    var dojo;
+    var dojo = "burbank";
+	dojo = "seattle";
+	console.log(dojo);
+	console.log(dojo);
+}
+console.log(dojo);
+learn();
+console.log(dojo);
