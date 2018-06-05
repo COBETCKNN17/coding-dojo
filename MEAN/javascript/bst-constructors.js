a867f0e479de233bd9e20a56781ec8074c3954d4
Nat <nuku@milkgoblin.com>
2018-06-05 12:17:18 -0500

    Added BST Traversals

--- MEAN/javascript/bst-constructors.js
+++ MEAN/javascript/bst-constructors.js
@@ -19,7 +19,7 @@ BST.prototype.insert = function (val) {
             if (walker.value < val) {
                 if (!walker.right) {
                     walker.right = new Node(val);
-                    return;
+                    return this;
                 }
                 else {
                     walker = walker.right;
@@ -29,7 +29,7 @@ BST.prototype.insert = function (val) {
             if (walker.value >= val) {
                 if (!walker.left) {
                     walker.left = new Node(val);
-                    return;
+                    return this;
                 }
                 else {
                     walker = walker.left;
@@ -39,13 +39,67 @@ BST.prototype.insert = function (val) {
     }
 }
 
+BST.prototype.postOrder = function () {
+    var tree = [this.root];
+    var reverseTree = [];
+    while (tree.length) {
+        var current = tree.pop();
+        reverseTree.push(current);
+
+        if (current.left) {
+            tree.push(current.left);
+        }
+        if (current.right) {
+            tree.push(current.right);
+        }
+    }
+    for (var i = reverseTree.length - 1; i >= 0; i--) {
+        console.log(reverseTree[i].value);
+    }
+}
+
+BST.prototype.preOrder = function () {
+    var tree = [this.root];
+
+    while (tree.length) {
+        var current = tree.pop();
+        console.log(current.value)
+        if (current.right) {
+            tree.push(current.right);
+        }
+        if (current.left) {
+            tree.push(current.left);
+        }
+    }
+}
+
+BST.prototype.inOrder = function(){
+    var tree = [this.root];
+    while (tree.length) {
+        var current = tree.pop();
+        if (current.left && !current.left.visited) {
+            tree.push(current);
+            tree.push(current.left);
+        } 
+        else {
+            current.visited = true;
+            console.log(current.value);
+            if (current.right) {
+                tree.push(current.right);
+            }
+        }
+    }
+}
+
+
 var first = new BST();
-first.root = new Node(30);
-first.root.right = new Node(50);
-first.root.left = new Node(10);
-first.insert(22);
-first.insert(200);
-first.insert(32);
-first.insert(65);
+first.root = new Node(40);
+first.insert(20).insert(25).insert(50);
 
 console.log(first);
+
+first.postOrder();
+console.log("---");
+first.preOrder();
+console.log("---");
+first.inOrder();
