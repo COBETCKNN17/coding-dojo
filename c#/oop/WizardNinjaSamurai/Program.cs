dd8ccb5384f986669329e2a5a0e24b6cd49f43b9
Nat <nuku@milkgoblin.com>
2018-07-03 11:15:18 -0500

    Wizard, Ninja, Samurai

---
+++ c#/oop/WizardNinjaSamurai/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WizardNinjaSamurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wizard Nat = new Wizard("Nat");
+            Ninja Josh = new Ninja("Josh");
+            Human Balazs = new Human("Balazs");
+            Samurai Will = new Samurai("Will");
+
+            Nat.Attack(Josh);
+            Nat.Fireball(Josh);
+            Josh.Steal(Nat);
+            Will.Attack(Balazs);
+            Josh.getAway();
+            Nat.Heal();
+            Will.Meditate();
+            Will.deathBlow(Nat);
+            Will.deathBlow(Josh);
+        }
+    }
+}
